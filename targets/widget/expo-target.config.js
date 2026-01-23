c0399234cbbf4fba059fe17e3ef159e5f94ec591
francesco-saponaro <francescosaponaro5@gmail.com>
2026-01-23 14:13:28 +0100

    again

--- targets/widget/expo-target.config.js
+++ targets/widget/expo-target.config.js
@@ -2,5 +2,7 @@
 module.exports = (config) => ({
   type: "widget",
   icon: "../../assets/images/main.png",
-  entitlements: "./expo-target.entitlements",
+  entitlements: {
+    "com.apple.security.application-groups": ["group.com.brainguin.app"],
+  },
 });
