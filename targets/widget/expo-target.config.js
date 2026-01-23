568405a3c992eeb07916c68022c878072d9a3ecc
francesco-saponaro <francesco.s@zumbat.it>
2026-01-23 19:15:47 +0100

    fix target vwrsioj

--- targets/widget/expo-target.config.js
+++ targets/widget/expo-target.config.js
@@ -2,6 +2,8 @@
 module.exports = (config) => ({
   type: "widget",
   icon: "https://github.com/expo.png",
+  deploymentTarget: "16.0", // Safe for iOS 17.5
+  exportJs: false, // <--- ADD THIS! Official docs recommend it for widgets.
   entitlements: {
     "com.apple.security.application-groups":
       config.ios.entitlements["com.apple.security.application-groups"],
